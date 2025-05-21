b97107d70c95bdcf2da39f38434699b5bf8722f7
Peterson William <petersondev.profissional@gmail.com>
2025-05-20 22:05:51 -0300

    Finalizado

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter as Router } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <Router>
-    <App />
-  </Router>,
+    <App />,
 )
